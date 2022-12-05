c977e7f93b92989373aa0f91f34b50c20d3f1ad9
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2022-12-05 14:37:21 +0000

    clean up
    Co-authored-by: LouisWarren <LouisWarren@users.noreply.github.com>

--- core/src/com/devcharles/piazzapanic/gameobjects/Player.java
+++ core/src/com/devcharles/piazzapanic/gameobjects/Player.java
@@ -1,7 +1,5 @@
 package com.devcharles.piazzapanic.gameobjects;
 
-import java.security.Key;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Vector2;
@@ -15,39 +13,7 @@ public class Player {
     }
 
     public void interact() {
-        // speed is 0.7071 of left or right
-        // if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-        //     if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-        //         currentCook.drawX -= 424 * Gdx.graphics.getDeltaTime();
-        //         currentCook.drawY -= 424* Gdx.graphics.getDeltaTime();
-            
-        //     } else if(Gdx.input.isKeyPressed(Keys.UP)) {
-        //         currentCook.drawX -= 424 * Gdx.graphics.getDeltaTime();
-        //         currentCook.drawY += 424 * Gdx.graphics.getDeltaTime();
-            
-        //     } else{
-        //         currentCook.drawX -= 600 * Gdx.graphics.getDeltaTime();
-        //     }
-        // }
-        // else if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-        //     if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-        //         currentCook.drawX += 424 * Gdx.graphics.getDeltaTime();
-        //         currentCook.drawY -= 424 * Gdx.graphics.getDeltaTime();
-            
-        //     } else if(Gdx.input.isKeyPressed(Keys.UP)) {
-        //         currentCook.drawX += 424 * Gdx.graphics.getDeltaTime();
-        //         currentCook.drawY += 424 * Gdx.graphics.getDeltaTime();
-            
-        //     } else{
-        //         currentCook.drawX += 600 * Gdx.graphics.getDeltaTime();
-        //     }
-        // }
-        // else if(Gdx.input.isKeyPressed(Keys.DOWN)) 
-        //     currentCook.drawY -= 600 * Gdx.graphics.getDeltaTime();
-        // else if(Gdx.input.isKeyPressed(Keys.UP)) 
-        //     currentCook.drawY += 600 * Gdx.graphics.getDeltaTime();
-
-
+        
         Vector2 v = new Vector2(0, 0);
         if (Gdx.input.isKeyPressed(Keys.LEFT)) {
             v.add(-1, 0);
