6a548ea26021c68d75637b479869e1ace8bdf840
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2023-01-29 19:15:43 +0000

    fix resizing and polish

--- core/src/com/devcharles/piazzapanic/scene2d/Hud.java
+++ core/src/com/devcharles/piazzapanic/scene2d/Hud.java
@@ -3,98 +3,130 @@ package com.devcharles.piazzapanic.scene2d;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.devcharles.piazzapanic.GameScreen;
 
 public class Hud extends ApplicationAdapter {
     public Stage gameStage;
     private Viewport gameViewport;
-    private Integer customerTimer;
-    private float timeCounter;
-    private Integer reputation;
+    private Integer customerTimer = 000;
+    private float timeCounter = 0;
+    private Integer reputation = 0;
     private Skin skin;
 
     // A label is basically a widget
+    LabelStyle hudLabelStyle;
     Label timerLabel;
     Label timeNameLabel;
     Label reputationLabel;
     Label reputationNameLabel;
-    BitmapFont gamesFont;
+    BitmapFont uiFont;
 
-    public Hud (SpriteBatch spriteBatch, final GameScreen savedGame,final Game game ) {
-        customerTimer = 000;
-        timeCounter = 0;
-        reputation = 3;
+    private Image photo, photo2;
 
-        gameViewport = new FitViewport(1280, 720, new OrthographicCamera());
+    private Game game;
+
+    public Hud(SpriteBatch spriteBatch, final GameScreen savedGame, final Game game) {
+        this.game = game;
+
+        gameViewport = new ScreenViewport(new OrthographicCamera(1280, 720));
         gameStage = new Stage(gameViewport, spriteBatch);
+        gameViewport.apply();
+
         skin = new Skin(Gdx.files.internal("craftacular/skin/craftacular-ui.json"));
-        Gdx.input.setInputProcessor(gameStage);
 
-        Table gameTable = new Table();
-        gameTable.top();
-        gameTable.setFillParent(true);
-        gameStage.addActor(gameTable);
+        hudLabelStyle = new Label.LabelStyle();
+        uiFont = new BitmapFont(Gdx.files.internal("craftacular/raw/font-export.fnt"));
+        hudLabelStyle.font = uiFont;
 
-        Label.LabelStyle hudLabelStyle = new Label.LabelStyle();
-        gamesFont = new BitmapFont(Gdx.files.internal("craftacular/raw/font-export.fnt"));
-        hudLabelStyle.font = gamesFont;
+        createTables(savedGame);
+    }
+
+    private void createTables(final GameScreen savedGame) {
 
         timerLabel = new Label(String.format("%03d", customerTimer), hudLabelStyle);
         reputationLabel = new Label(String.format("%01d", reputation), hudLabelStyle);
         timeNameLabel = new Label("Time", hudLabelStyle);
         reputationNameLabel = new Label("Reputation", hudLabelStyle);
 
-        gameTable.add(timeNameLabel).expandX().padTop(10);
-        gameTable.add(reputationNameLabel).expandX().padTop(10);
-        gameTable.row();
-        gameTable.add(timerLabel).expandX();
-        gameTable.add(reputationLabel).expandX();
-        gameTable.row();
         TextButton recipeBookButton = new TextButton("Recipe Book", skin);
-        gameTable.add(recipeBookButton).width(220).height(50).left().top();
-        gameTable.row();
         TextButton tutorialButton = new TextButton("Tutorial", skin);
-        gameTable.add(tutorialButton).width(220).height(50).left().top();
 
-
-        recipeBookButton.addListener(new ClickListener(){
+        recipeBookButton.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
-                        game.setScreen(new recipeBook(0, savedGame, game));
+                game.setScreen(new recipeBook(0, savedGame, game));
             }
         });
-        tutorialButton.addListener(new ClickListener(){
+        tutorialButton.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new Tutorial(0, savedGame, game, 0));
             }
         });
 
+        // We will need to update these to food textures.
+        photo = new Image(new Texture(Gdx.files.internal("droplet.png")));
+        photo2 = new Image(new Texture(Gdx.files.internal("droplet.png")));
+
+        Table tableLeft = new Table();
+        tableLeft.top();
+        tableLeft.setFillParent(true);
+
+        tableLeft.add(timeNameLabel).expandX().padTop(10);
+        tableLeft.add(reputationNameLabel).expandX().padTop(10);
+        tableLeft.row();
+        tableLeft.add(timerLabel).expandX();
+        tableLeft.add(reputationLabel).expandX();
+        tableLeft.row();
+
+        tableLeft.add(recipeBookButton).width(220).height(50).left().top();
+        tableLeft.row();
+        tableLeft.add(tutorialButton).width(220).height(50).left().top();
+
+        Table tableRight = new Table();
+        tableRight.right();
+        tableRight.setFillParent(true);
+
+        tableRight.add(photo).expandX().right();
+        tableRight.row();
+        tableRight.add(photo2).expandX().right();
+
+        gameStage.addActor(tableLeft);
+        gameStage.addActor(tableRight);
+
     }
 
-    public void update(float deltaTime){
+    public void update(float deltaTime) {
         timeCounter += deltaTime;
-        if (timeCounter >= 1){
-            customerTimer ++;
+        if (timeCounter >= 1) {
+            customerTimer++;
             timerLabel.setText(String.format("%03d", customerTimer));
             timeCounter = 0;
         }
+
         gameStage.act();
         gameStage.draw();
 
     }
-    public void dispose(){
-        gameStage.dispose();
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        gameViewport.update(width, height, true);
+        gameViewport.apply();
     }
 
+    public void dispose() {
+        gameStage.dispose();
+    }
 
 }
