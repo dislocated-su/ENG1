8785d490c6b9d5ef3eb49b02489c71da28e8456f
Matt Rohatynskyj <mjr593@york.ac.uk>
2023-02-22 16:45:26 +0000

    Added JUnit Testing

---
+++ tests/src/com/devcharles/piazzapanic/AssetTests.java
@@ -0,0 +1,15 @@
+package com.devcharles.piazzapanic;
+
+import static org.junit.Assert.assertTrue;
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+
+    @Test
+    public void testDropeletAssetExists() throws Exception {
+        assertTrue("Check existance of droplet assets", Gdx.files.internal("v2/food.png").exists());
+    }
+}
