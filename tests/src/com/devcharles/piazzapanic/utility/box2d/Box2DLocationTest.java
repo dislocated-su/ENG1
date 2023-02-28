491bc7e62be28ddfe01f2e7c6393ec5f410acf65
Alistair Foggin <alistair.foggin02@gmail.com>
2023-02-28 11:05:09 +0000

    Refactor GdxTimerTest to be in correct package, add delta to assertEquals and reformat

--- tests/src/com/devcharles/piazzapanic/utility/box2d/Box2DLocationTest.java
+++ tests/src/com/devcharles/piazzapanic/utility/box2d/Box2DLocationTest.java
@@ -11,27 +11,37 @@ import org.junit.runner.RunWith;
 @RunWith(GdxTestRunner.class)
 public class Box2DLocationTest {
 
-    @Test
-    public void newLocationTest(){
-        Box2dLocation location = new Box2dLocation(new Vector2(5,10),-1);
-        Location<Vector2> newLocation = location.newLocation();
-        assertEquals("Expect new location to have vector of (0,0).",new Vector2(0,0), newLocation.getPosition());
-        assertEquals("Expect new location to have orientation of 0.",0, newLocation.getOrientation(),0);
-    }
+  @Test
+  public void newLocationTest() {
+    Box2dLocation location = new Box2dLocation(new Vector2(5, 10), -1);
+    Location<Vector2> newLocation = location.newLocation();
+    assertEquals("Expect new location to have vector of (0,0).", new Vector2(0, 0),
+        newLocation.getPosition());
+    assertEquals("Expect new location to have orientation of 0.", 0, newLocation.getOrientation(),
+        0.00001);
+  }
 
-    @Test
-    public void vectorToAngleTest(){
-        Box2dLocation location = new Box2dLocation(new Vector2(),0);
-        assertEquals("Expect calculated angle to be correct.",(float) -0.78539816,location.vectorToAngle(new Vector2(5,5)),0);
-        assertEquals("Expect calculated angle to be correct with negative x.",(float) 1.10714872,location.vectorToAngle(new Vector2(-10,5)),0);
-        assertEquals("Expect calculated angle to be correct with negative y.",(float) -1.9513027,location.vectorToAngle(new Vector2(25,-10)),0);
-        assertEquals("Expect calculated angle to be correct with negative x and y.",(float) 1.735945,location.vectorToAngle(new Vector2(-30,-5)),0);
-    }
+  @Test
+  public void vectorToAngleTest() {
+    Box2dLocation location = new Box2dLocation(new Vector2(), 0);
+    assertEquals("Expect calculated angle to be correct.", (float) -0.78539816,
+        location.vectorToAngle(new Vector2(5, 5)), 0.00001);
+    assertEquals("Expect calculated angle to be correct with negative x.", (float) 1.10714872,
+        location.vectorToAngle(new Vector2(-10, 5)), 0.00001);
+    assertEquals("Expect calculated angle to be correct with negative y.", (float) -1.9513027,
+        location.vectorToAngle(new Vector2(25, -10)), 0.00001);
+    assertEquals("Expect calculated angle to be correct with negative x and y.", (float) 1.735945,
+        location.vectorToAngle(new Vector2(-30, -5)), 0.00001);
+  }
 
-    @Test
-    public void angleToVectorTest(){
-        Box2dLocation location = new Box2dLocation(new Vector2(),0);
-        assertEquals("Expect calculated vector to be correct.",new Vector2((float) -0.70710678, (float) 0.70710678), location.angleToVector(new Vector2 (), (float) 0.78539816));
-        assertEquals("Expect calculated vector to be correct with negative angle.", new Vector2((float) 0.70710678, (float) -0.70710678), location.angleToVector(new Vector2 (), (float) -2.35619449));
-    }
+  @Test
+  public void angleToVectorTest() {
+    Box2dLocation location = new Box2dLocation(new Vector2(), 0);
+    assertEquals("Expect calculated vector to be correct.",
+        new Vector2((float) -0.70710678, (float) 0.70710678),
+        location.angleToVector(new Vector2(), (float) 0.78539816));
+    assertEquals("Expect calculated vector to be correct with negative angle.",
+        new Vector2((float) 0.70710678, (float) -0.70710678),
+        location.angleToVector(new Vector2(), (float) -2.35619449));
+  }
 }
