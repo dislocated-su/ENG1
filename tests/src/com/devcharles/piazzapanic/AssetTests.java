37a0081508eb628509ca389e5d254265f1dd682a
Matt Rohatynskyj <mjr593@york.ac.uk>
2023-03-02 17:44:20 +0000

    few extra asset tests

--- tests/src/com/devcharles/piazzapanic/AssetTests.java
+++ tests/src/com/devcharles/piazzapanic/AssetTests.java
@@ -9,7 +9,32 @@ import org.junit.runner.RunWith;
 public class AssetTests {
 
     @Test
-    public void testDropletAssetExists() throws Exception {
-        assertTrue("Check existance of droplet assets", Gdx.files.internal("v2/food.png").exists());
+    public void testFoodAssetsExists() throws Exception {
+        assertTrue("Check existance of food assets", Gdx.files.internal("v2/food.png").exists());
+    }
+
+    @Test
+    public void testTilemapAssetsExists() throws Exception {
+        assertTrue("Check existance of tmx file", Gdx.files.internal("v2/map.tmx").exists());
+        assertTrue("Check existance of tilemap objects tileset", Gdx.files.internal("v2/objects.tsx").exists());
+        assertTrue("Check existance of tilemap tileset_32 tileset", Gdx.files.internal("v2/objects.tsx").exists());
+    }
+
+    @Test
+    public void testTutorialAssetsExists() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            assertTrue(String.format("Check existance of tutorial%d.png file", i),
+                    Gdx.files.internal(String.format("tutorial%d.png", i)).exists());
+        }
+    }
+
+    @Test
+    public void testCustomerAssetsExists() throws Exception {
+        for (int i = 1; i < 15; i++) {
+            assertTrue(String.format("Check existance of customer %d.png file", i),
+                    Gdx.files.internal(String.format("v2/customer/%d.png", i)).exists());
+            assertTrue(String.format("Check existance of customer %d_holding.png file", i),
+                    Gdx.files.internal(String.format("v2/customer/%d_holding.png", i)).exists());
+        }
     }
 }
