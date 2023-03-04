6ef6dfd6d62bacd29b05734fe22755521fc16888
Alistair Foggin <alistair.foggin02@gmail.com>
2023-03-04 16:20:21 +0000

    Revert failing test

--- tests/src/com/devcharles/piazzapanic/AssetTest.java
+++ tests/src/com/devcharles/piazzapanic/AssetTest.java
@@ -14,9 +14,4 @@ public class AssetTest {
     assertTrue("This test will only pass when mainMenuImage.png exists",
         Gdx.files.internal("mainMenuImage.png").exists());
   }
-
-  @Test
-  public void testShouldFail() {
-    fail("This should fail.");
-  }
 }
