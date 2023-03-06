13fa552e50997f01a8a1be188de889198e7510d7
Alistair Foggin <alistair.foggin02@gmail.com>
2023-03-06 15:06:51 +0000

    Test createCook and createFood

--- tests/src/com/devcharles/piazzapanic/utility/EntityFactoryTest.java
+++ tests/src/com/devcharles/piazzapanic/utility/EntityFactoryTest.java
@@ -2,8 +2,23 @@ package com.devcharles.piazzapanic.utility;
 
 import static org.junit.Assert.*;
 
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.devcharles.piazzapanic.GdxTestRunner;
+import com.devcharles.piazzapanic.components.AnimationComponent;
+import com.devcharles.piazzapanic.components.B2dBodyComponent;
+import com.devcharles.piazzapanic.components.ControllableComponent;
+import com.devcharles.piazzapanic.components.FoodComponent;
 import com.devcharles.piazzapanic.components.FoodComponent.FoodType;
+import com.devcharles.piazzapanic.components.TextureComponent;
+import com.devcharles.piazzapanic.components.TransformComponent;
+import com.devcharles.piazzapanic.components.WalkingAnimationComponent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -11,11 +26,44 @@ import org.junit.runner.RunWith;
 public class EntityFactoryTest {
 
   @Test
-  public void createCook() {
+  public void testCreateCook() {
+    PooledEngine engine = new PooledEngine();
+    World world = new World(Vector2.Zero, true);
+    EntityFactory factory = new EntityFactory(engine, world);
+
+    Entity cook = factory.createCook(0, 0);
+    ImmutableArray<Entity> entities = engine.getEntitiesFor(
+        Family.all(B2dBodyComponent.class, TransformComponent.class,
+            ControllableComponent.class, TextureComponent.class, AnimationComponent.class,
+            WalkingAnimationComponent.class).get());
+
+    assertEquals("There should be one entity that has the cook components.", 1,
+        entities.size());
+    assertEquals("The cook should be the same as what is in the engine.", cook, entities.get(0));
+
+    Array<Fixture> fixtures = new Array<>();
+    world.getFixtures(fixtures);
+    Fixture cookFixture = fixtures.first();
+
+    assertNotNull("There should be a cook fixture.", cookFixture);
+    assertEquals("The cook entity should be added as the user data in the fixture.", cook,
+        cookFixture.getUserData());
   }
 
   @Test
   public void createFood() {
+    PooledEngine engine = new PooledEngine();
+    World world = new World(Vector2.Zero, true);
+    EntityFactory factory = new EntityFactory(engine, world);
+
+    Entity food = factory.createFood(FoodType.burger);
+    ImmutableArray<Entity> entities = engine.getEntitiesFor(
+        Family.all(TextureComponent.class, TransformComponent.class, FoodComponent.class).get());
+
+    assertEquals("There should be one entity that has the food components.", 1,
+        entities.size());
+    assertEquals("The food should be the same as what is in the engine.", food, entities.get(0));
+    assertEquals("The food type should be burger.", FoodType.burger, Mappers.food.get(food).type);
   }
 
   @Test
